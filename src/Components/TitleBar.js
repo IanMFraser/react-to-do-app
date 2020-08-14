0f6553291916c61ed165f03b041608a3dd96c062
IanMFraser <hunter_gracchus@hotmail.com>
2020-08-14 15:34:43 -0400

    useHistory

--- src/Components/TitleBar.js
+++ src/Components/TitleBar.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 const TitleBar = ({newTitle, titleHandler, deleteFormHandler}) => {
+    let history = useHistory();
     return( 
         <div className="title">
-            <button><FontAwesomeIcon icon={faArrowLeft}/></button>
+            <button onClick={history.goBack}><FontAwesomeIcon icon={faArrowLeft}/></button>
             <input type="text" placeholder="To Do" value={newTitle} onChange={titleHandler}/>
             <button onClick={deleteFormHandler}><FontAwesomeIcon icon={faTrash}/></button>
         </div>
