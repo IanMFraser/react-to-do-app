a9b98a27b1bf9ae4090891dced5d16f819cc710b
IanMFraser <hunter_gracchus@hotmail.com>
2020-08-14 18:18:09 -0400

    cleaned up some styling

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -6,8 +6,8 @@ const MainPage = ({lists}) => {
     
     return(
         <div className="container">
-            <h1>To Do</h1>
-            <button>+</button>
+            <h1>To Do <button>+</button> </h1>
+            
             <DisplayLists lists={lists}/>
         </div>
     )
