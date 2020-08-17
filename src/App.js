ee0018f7b2a91def4c3a7d4a0c8da5097dff6889
IanMFraser <hunter_gracchus@hotmail.com>
2020-08-17 15:46:44 -0400

    add new list handlers to main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Db from './db.json';
+import React, { useState } from 'react';
+// import Db from './db.json';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MainPage from './Components/MainPage';
 import ToDoPage from './Components/ToDoPage';
@@ -7,14 +7,68 @@ import './App.css';
 
 const App = () => {
   
-  const toDoLists = Db["todos"];
+  const [toDoLists, setToDoLists] = useState([
+    {
+      "title": "'To Do App' To Do",
+      "items": [
+          {
+              "id": 0,
+              "items": "implement title-bar  component",
+              "isSelected": false
+          },
+          {
+              "id": 1,
+              "items": "refactor css to use flexbox and sass",
+              "isSelected": false
+          },
+          {
+              "id": 2,
+              "items": "install json server for external storage of lists",
+              "isSelected": false
+          }
+      ]
+    },
+    {
+      "title": "Errands",
+      "items": [
+          {
+              "id": 0,
+              "items": "laundry",
+              "isSelected": false
+          },
+          {
+              "id": 1,
+              "items": "cook lunch",
+              "isSelected": false
+          },
+          {
+              "id": 2,
+              "items": "clean litter box",
+              "isSelected": false
+          }
+      ]
+    }
+  ]) 
+  
+  // const history = useHistory()
+
+  const newListHandler = (e) => {
+    // e.preventDefault();
+    const newList = {
+      "title": "newToDo",
+      "items": []
+    }
+
+    console.log(newList["title"])
+    setToDoLists([...toDoLists, newList])
+  }
 
   return(
     <Router>
       <Switch>
         {toDoLists.map((list,i) => <Route key={i} path={`/${list["title"]}`} > <ToDoPage lists={list} /> </Route> )}
         <Route path='/'>
-          <MainPage lists={toDoLists} />
+          <MainPage lists={toDoLists} newListHandler={newListHandler} />
         </Route>
       </Switch>
     </Router>
--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import DisplayLists from './DisplayLists';
 import '../App.css';
+import { withRouter, useHistory } from 'react-router-dom';
 
-const MainPage = ({lists}) => {
+
+const MainPage = ({ lists, newListHandler }) => {
     
+    const history = useHistory()
+    const redirectTodo = () => {
+        history.push('/newToDo')
+    }
+
     return(
         <div className="container">
-            <h1>To Do <button>+</button> </h1>
-            
-            <DisplayLists lists={lists}/>
+            <div className="main-header">
+                <h1>To Do</h1>
+                <button onClick={ () => { newListHandler(); redirectTodo(); } }>+</button>
+            </div>
+            <DisplayLists lists={ lists }/>
         </div>
     )
 }
 
-export default MainPage;
+export default withRouter(MainPage);
