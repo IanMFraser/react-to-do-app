8fe2e54614b65aeb93485adca983a8994f120ffb
IanMFraser <hunter_gracchus@hotmail.com>
2020-10-22 13:59:44 -0400

    Updates

---
+++ precache-manifest.3fbfa9e61224963e66f5c4a0ef996d31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc21f8df785b57621bd2030f14c1468",
+    "url": "/react-to-do-app/index.html"
+  },
+  {
+    "revision": "d66d40f7287a62fa72d4",
+    "url": "/react-to-do-app/static/css/main.e5d13aaa.chunk.css"
+  },
+  {
+    "revision": "5e2969515a7d78f3a5b3",
+    "url": "/react-to-do-app/static/js/2.ce71624b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-to-do-app/static/js/2.ce71624b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d66d40f7287a62fa72d4",
+    "url": "/react-to-do-app/static/js/main.09eff9ac.chunk.js"
+  },
+  {
+    "revision": "5a345290e9e36071e7e1",
+    "url": "/react-to-do-app/static/js/runtime-main.e78f253b.js"
+  }
+]);
